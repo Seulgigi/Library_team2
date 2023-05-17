fef5ee3706a67a6a406d38c2ad61aa2670ace572
Seulgigi <2021112414@dgu.ac.kr>
2023-05-17 11:10:06 +0900

    [Feat] Main 프레임 추가

---
+++ Main.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import java.awt.*;
+// import java.awt.event.*;
+
+public class Main extends JFrame{
+    // 클래스 선언 자리
+
+    // 기본 프레임
+    public Main() {
+        setTitle("도서관 좌석 관리 시스템");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+        setSize(1000,800);
+        setLocationRelativeTo(null);
+		setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        // 메인 클래스 실행
+        Main main = new Main();
+
+        // 나머지 클래스 실행 자리
+    }
+}
